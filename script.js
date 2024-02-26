feb165b5ed49ab507acfe53c7872648a86c4cffa
johannalohmus <johanna.lohmus@gmail.com>
2024-02-26 17:54:47 -0500

    add sliders and colors

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-let startingWord = "said"
-let endingWord = "seed"
-let smallestNumberOfGuesses = ""
+var redSlider = document.getElementById("redSlider");
+var redOutput = document.getElementById("redOutput");
+redOutput.innerHTML = redSlider.value;
+
+redSlider.oninput = function () {
+    redOutput.innerHTML = this.value;
+}
+
+var greenSlider = document.getElementById("greenSlider");
+var greenOutput = document.getElementById("greenOutput");
+greenOutput.innerHTML = greenSlider.value;
+
+greenSlider.oninput = function () {
+    greenOutput.innerHTML = this.value;
+}
+
+var blueSlider = document.getElementById("blueSlider");
+var blueOutput = document.getElementById("blueOutput");
+blueOutput.innerHTML = blueSlider.value;
+
+blueSlider.oninput = function () {
+    blueOutput.innerHTML = this.value;
+}
